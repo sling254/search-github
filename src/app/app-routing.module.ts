0a25818f1f2397d7a804d88517859e19d2dfc732
sling254 <mwanikidenis7@gmail.com>
2021-10-16 11:48:32 +0300

    add a 404 error message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundComponent} from '../app/components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

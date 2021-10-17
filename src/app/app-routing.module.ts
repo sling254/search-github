fc4026517b65d0c39e66a9c553abacd267240e36
sling254 <mwanikidenis7@gmail.com>
2021-10-17 11:53:46 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {NotFoundComponent} from '../app/components/not-found/not-found.component
 import { GithubComponent } from './components/github/github.component';
 
 const routes: Routes = [
+  {path:"", component:GithubComponent},
+  {path:"**", component:NotFoundComponent}
   
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
